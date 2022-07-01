660869a7348857000c103dcb1902ca8bfd0fe1fa
IvanSanczewski <isanczewski@gmail.com>
2022-07-01 17:32:20 +0300

    Move products array in a separete products.js file

---
+++ js/products.js
@@ -0,0 +1,65 @@
+var products = [
+    {
+         id: 1,
+         name: 'Lituania',
+         price: 95,
+         type: 'copies',
+         offer: {
+             number: 3,
+             percent: 20
+         }
+     },
+     {
+         id: 2,
+         name: 'Odesa',
+         price: 175,
+         type: 'copies'
+ 
+     },
+     {
+         id: 3,
+         name: 'Bergamo',
+         price: 300,
+         type: 'copies',
+         offer: {
+             number: 5,
+             percent: 30
+         }
+     },
+     {
+         id: 4,
+         name: 'Wedding',
+         price: 1250,
+         type: 'events'
+     },
+     {
+         id: 5,
+         name: 'Portraiture Session',
+         price: 250,
+         type: 'events'
+     },
+     {
+         id: 6,
+         name: 'Corporate Event',
+         price: 400,
+         type: 'events'
+     },
+     {
+         id: 7,
+         name: 'Lightroom',
+         price: 95,
+         type: 'courses'
+     },
+     {
+         id: 8,
+         name: 'Photoshop',
+         price: 95,
+         type: 'courses'
+     },
+     {
+         id: 9,
+         name: 'Street Photography',
+         price: 135,
+         type: 'courses'
+     }
+ ]
